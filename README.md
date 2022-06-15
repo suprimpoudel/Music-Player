87bd238ba6e579c19b0a4bd2586b3c27bf497259
Suprim Poudel <suprim.poudel@ekbana.info>
2022-06-15 23:42:30 +0545

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# flutter_music_player
+
+Music Player Made using Flutter
+
+## Getting Started
+
+Switch to branches to view the source code
---
+++ android/app/src/main/kotlin/np/com/suprimpoudel/flutter_music_player/MainActivity.kt
@@ -0,0 +1,6 @@
+package np.com.suprimpoudel.flutter_music_player
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
